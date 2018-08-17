09fae0404955337859c9949d4bb39c853c159c10
padronlv <padronlv@gmail.com>
2018-08-17 13:53:24 +0200

    heroku socket

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server, { origins: 'localhost:8080' });
+const io = require('socket.io')(server, { origins: 'localhost:8080 chat-victorpl.herokuapp.com:*'  });
 const compression = require('compression');
 const db = require('./db/db');
 const cookieParser = require('cookie-parser');
